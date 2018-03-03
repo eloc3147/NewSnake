3f5819c2422702d9682b7b25073c24b5bd515653
Kinnon McAlister <kinnonmcalister@gmail.com>
2018-03-03 11:09:40 -0800

    Updated snake for 2018 server

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ from collections import defaultdict
 from pypaths import astar
 import json
 
+FOOD = 2
+YOU_HEAD = 21
+YOU_BODY = 20
+SNAKE_HEAD = 11
+SNAKE_BODY = 10
+
 @bottle.route('/static/<path:path>')
 def static(path):
     return bottle.static_file(path, root='static/')
@@ -32,68 +38,78 @@ def start():
 
 @bottle.post('/move')
 def move():
-    data = bottle.request.json
+    data = dict(bottle.request.json)
     width = data['width']
     height = data['height']
     grid = [[0 for x in range(width)] for y in range(height)] 
-    snakes = data['snakes']
-    food = data['food']
-    snake = {}
-    
+    snakes = data['snakes']['data']
+    food = [ptoc(x) for x in data['food']['data']]
+    snake = data['you']
+
+    # Add this snake to grid
+    for idx, point in enumerate(snake['body']['data']):
+        x = point['x']
+        y = point['y']
+        if grid[x][y] != 0:
+            # Stops snake bodies from overwriting heads at the beginning
+            continue
+        # If this is the forst coordinate, it's the head
+        if idx == 0:
+            grid[x][y] = YOU_HEAD
+        else:
+            grid[x][y] = YOU_BODY
+    # Add other snakes to grid
     for s in snakes:
-        you = False
-        if s['id'] == data['you']:
-            snake = s
-            you = True
-        for idx, val in enumerate(s['coords']):
-            x, y = val
+        # Skip snake if dead
+        if(s['health'] < 1):
+            continue
+        for idx, point in enumerate(s['body']['data']):
+            x = point['x']
+            y = point['y']
             if grid[x][y] != 0:
-                continue # Stops snake bodies from overwriting heads at the beginning
+                # Stops snake bodies from overwriting heads at the beginning
+                continue
             # If this is the forst coordinate, it's the head
             if idx == 0:
-                grid[x][y] = 11 if you else 21
+                grid[x][y] = SNAKE_HEAD
             else:
-                grid[x][y] = 10 if you else 20
-    for coords in food:
-        x, y = coords
-        grid[x][y] = 2
-        
-    head = snake['coords'][0]
-        
+                grid[x][y] = SNAKE_BODY
+    for c in food:
+        x, y = c
+        grid[x][y] = FOOD
+
+    head = ptoc(snake['body']['data'][0])
+
     # Simple macros for each direction
-    c_north = [snake['coords'][0][0], snake['coords'][0][1] - 1]
-    c_south = [snake['coords'][0][0], snake['coords'][0][1] + 1]
-    c_east = [snake['coords'][0][0] + 1, snake['coords'][0][1]]
-    c_west = [snake['coords'][0][0] - 1, snake['coords'][0][1]]
-    
-    #Check if a given coordiante is safe
-    def coords_safe(coords):
-        x, y = coords
-        if x < 0 or x > width-1: return False # Check if coordinate is inside horizontal bounds
-        if y < 0 or y > height-1: return False # Check if coordinate is inside vertical bounds
-        if grid[x][y] not in [0,2]: return False # Check if coordinate matches a snake body
-        return True
-    
+    c_north = [head[0], head[1] - 1]
+    c_south = [head[0], head[1] + 1]
+    c_east = [head[0] + 1, head[1]]
+    c_west = [head[0] - 1, head[1]]
+
     def find_neighbours(coord):
         x, y = coord
         neighbors = []
-        
-        if coords_safe([x-1, y]): neighbors.append((x-1, y))
-        if coords_safe([x, y-1]): neighbors.append((x, y-1))
-        if coords_safe([x+1, y]): neighbors.append((x+1, y))
-        if coords_safe([x, y+1]): neighbors.append((x, y+1))
-        
+
+        if coords_safe([x-1, y], width, height, grid):
+            neighbors.append((x-1, y))
+        if coords_safe([x, y-1], width, height, grid):
+            neighbors.append((x, y-1))
+        if coords_safe([x+1, y], width, height, grid):
+            neighbors.append((x+1, y))
+        if coords_safe([x, y+1], width, height, grid):
+            neighbors.append((x, y+1))
+
         return neighbors
-    
+
     finder = astar.pathfinder(neighbors=find_neighbours)
-    path = finder( (head[0], head[1]), (food[0][0], food[0][1]) )[1]
-    
+    path = finder((head[0], head[1]), (food[0][0], food[0][1]))[1]
+
     if len(path) < 2:
-        if coords_safe(c_north):
+        if coords_safe(c_north, width, height, grid):
             direction = "up"
-        elif coords_safe(c_south):
+        elif coords_safe(c_south, width, height, grid):
             direction = "down"
-        elif coords_safe(c_east):
+        elif coords_safe(c_east, width, height, grid):
             direction = "right"
         else:
             direction = "left"
@@ -114,7 +130,26 @@ def move():
     }
 
 
+# Check if a given coordiante is safe
+def coords_safe(coords, width, height, grid):
+    x, y = coords
+    # Check if coordinate is inside horizontal bounds
+    if(x < 0 or x > width-1):
+        return False
+    # Check if coordinate is inside vertical bounds
+    if(y < 0 or y > height-1):
+        return False
+    # Check if coordinate matches a snake body
+    if(grid[x][y] not in [0, 2]):
+        return False
+    return True
+
+
+def ptoc(point):
+    return [point['x'], point['y']]
+
 # Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
 if __name__ == '__main__':
-    bottle.run(application, host=os.getenv('IP', '0.0.0.0'), port=os.getenv('PORT', '8080'))
+    bottle.run(application, host=os.getenv('IP', '0.0.0.0'),
+               port=os.getenv('PORT', '8080'))
